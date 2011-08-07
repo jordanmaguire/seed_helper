ef9cda79598219834119ff60fad62c9749c14e7b
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2011-08-07 20:14:40 +0800

    DRY it up

--- lib/seed_formatter.rb
+++ lib/seed_formatter.rb
@@ -4,26 +4,26 @@ require "colored"
 module SeedFormatter
   class << self
     ## Colored.colors is an array of strings representing the available colours.
-    def output message, color, options
-      puts "#{options[:prefix]}#{message}#{options[:suffix]}".send(color)
+    def output message, options
+      puts "#{options[:prefix]}#{message}#{options[:suffix]}".send(optoins[color])
     end
   
     def message message, options = {}
       options[:prefix] ||= "*** "
       options[:color] ||= :white
-      output message, options[:color], options
+      output message, options
     end
     
     def success message, options = {}
       options[:prefix] ||= "  + "
       options[:color] ||= :green
-      output message, options[:color], options
+      output message, options
     end
     
     def error message, options = {}
       options[:prefix] ||= "  - "
       options[:color] ||= :red
-      output message, options[:color], options
+      output message, options
     end
   end
 end
