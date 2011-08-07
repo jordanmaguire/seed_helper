1734330263443889c37994cfd14dccfd38cdcd74
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2011-08-07 20:13:22 +0800

    First cut at the three primary formatters.

--- lib/seed_formatter.rb
+++ lib/seed_formatter.rb
@@ -1,5 +1,29 @@
 require "seed_formatter/version"
+require "colored"
 
 module SeedFormatter
-  # Your code goes here...
+  class << self
+    ## Colored.colors is an array of strings representing the available colours.
+    def output message, color, options
+      puts "#{options[:prefix]}#{message}#{options[:suffix]}".send(color)
+    end
+  
+    def message message, options = {}
+      options[:prefix] ||= "*** "
+      options[:color] ||= :white
+      output message, options[:color], options
+    end
+    
+    def success message, options = {}
+      options[:prefix] ||= "  + "
+      options[:color] ||= :green
+      output message, options[:color], options
+    end
+    
+    def error message, options = {}
+      options[:prefix] ||= "  - "
+      options[:color] ||= :red
+      output message, options[:color], options
+    end
+  end
 end
